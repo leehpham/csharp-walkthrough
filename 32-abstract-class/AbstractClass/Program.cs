5e28199d9f881ae2582d3bd61ad15179dd67eee8
Hoang Pham <lehoangpham1092@gmail.com>
2019-03-25 22:38:20 -0500

    ...

---
+++ 32-abstract-class/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    public abstract class Customer
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print");
+        }
+    }
+
+    public class Program : Customer
+    {        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
