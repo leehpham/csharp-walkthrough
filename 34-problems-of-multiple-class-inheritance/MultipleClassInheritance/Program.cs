6390cbf3a6f4f8848cd991923f787d9b002f189b
Hoang Pham <lehoangpham1092@gmail.com>
2019-03-28 07:17:46 -0500

    ...

---
+++ 34-problems-of-multiple-class-inheritance/MultipleClassInheritance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MultipleClassInheritance
+{
+    class A
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("A Implementation");
+        }
+    }
+
+    class B : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("B Implementation");
+        }
+    }
+
+    class C : A
+    {
+        public override void Print()
+        {
+            Console.WriteLine("C Implementation");
+        }
+    }
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 36-delegates/DelegateExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DelegateExample
+{
+    public delegate void HelloFunctionDelegate(string message);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A delegate is a type safe function pointer
+            HelloFunctionDelegate myDelegate = new HelloFunctionDelegate(Hello);
+            myDelegate("Hello from Delegate");
+        }
+
+        public static void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+        }
+    }
+}
