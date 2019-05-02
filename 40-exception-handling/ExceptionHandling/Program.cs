635034d73792bc768cd43f93addcbbd2762dd80f
Hoang Pham <lehoangpham1092@gmail.com>
2019-05-01 22:19:42 -0500

    ...

---
+++ 40-exception-handling/ExceptionHandling/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a StreamReader to read from a file.
+            StreamReader streamReader = new StreamReader();
+        }
+    }
+}
