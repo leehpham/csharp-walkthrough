43ff0d83350c5d25e5f9f088dab017824bbf7b5d
Hoang Pham <lehoangpham1092@gmail.com>
2019-05-25 12:12:59 -0500

    ...

--- 40-exception-handling/ExceptionHandling/Program.cs
+++ 40-exception-handling/ExceptionHandling/Program.cs
@@ -7,8 +7,32 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            // Create a StreamReader to read from a file.
-            StreamReader streamReader = new StreamReader();
+            StreamReader streamReader = null;
+            try
+            {
+                // Create a StreamReader to read from a file.
+                streamReader = new StreamReader(@"D:\hoang\csharp-walkthrough\40-exception-handling\ExceptionHandling\data.txt");
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"Please check if the file {ex.FileName} exists");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                Console.WriteLine("Finally Block");
+            }
+
+            Console.WriteLine("Get here anw");
         }
     }
 }
