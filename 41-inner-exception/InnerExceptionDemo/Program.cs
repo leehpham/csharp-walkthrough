db5f814f75c7c0470bf93cee9dea200f35e34cab
Hoang Pham <lehoangpham1092@gmail.com>
2020-03-30 22:46:23 -0500

    ...

---
+++ 41-inner-exception/InnerExceptionDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace InnerExceptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    Console.WriteLine("Enter First Number");
+                    int fn = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter Second Number");
+                    int sn = Convert.ToInt32(Console.ReadLine());
+
+                    int result = fn / sn;
+
+                    Console.WriteLine($"Result = {result}");
+                }
+                catch (Exception ex)
+                {
+                    string filePath = @"D:\hoang\git-repo\Log1.txt";
+                    if (File.Exists(filePath))
+                    {
+                        StreamWriter sw = new StreamWriter(filePath);
+                        sw.WriteLine(ex.GetType().Name);
+                        sw.WriteLine(ex.Message);
+                        sw.Close();
+                        Console.WriteLine("There is a problem, please try later.");
+                    }
+                    else 
+                    {
+                        throw new FileNotFoundException(filePath + " is not present", ex);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                
+            }
+        }
+    }
+}
